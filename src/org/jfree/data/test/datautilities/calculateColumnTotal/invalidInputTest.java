fc57c5dd5854079c928b36cb1084f9d5fb3c3c2f
Carguy18 <aaronkobelsky@gmail.com>
2017-02-10 15:17:39 -0700

    Added another test case

---
+++ src/org/jfree/data/test/datautilities/calculateColumnTotal/invalidInputTest.java
@@ -0,0 +1,14 @@
+package org.jfree.data.test.datautilities.calculateColumnTotal;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class invalidInputTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
