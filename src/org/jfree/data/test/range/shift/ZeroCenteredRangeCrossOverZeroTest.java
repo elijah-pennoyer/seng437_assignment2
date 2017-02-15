c9402a7770596651b835d8e49dd1400151b96ad7
Heather <heather.honecker@ucalgary.ca>
2017-02-15 15:24:53 -0700

    Added another CrossOverZero test to shift

--- src/org/jfree/data/test/range/shift/ZeroCenteredRangeCrossOverZeroTest.java
+++ src/org/jfree/data/test/range/shift/ZeroCenteredRangeCrossOverZeroTest.java
@@ -6,7 +6,7 @@ import org.jfree.data.Range;
 import org.junit.Before;
 import org.junit.Test;
 
-public class CrossOverZeroTest {
+public class ZeroCenteredRangeCrossOverZeroTest {
 
 	public Range r;
 
@@ -20,14 +20,14 @@ public class CrossOverZeroTest {
 	
 	/**
 	 * Test Plan Coverage: shift test plan 6
-	 * Test Strategy Coverage: attempt to cross over zero
+	 * Test Strategy Coverage: attempt to cross over zero with a zero-centered range
 	 * Description: Tests the shift(Range, double) function on a Range from -1.0 to 1.0
 	 * 				with delta = 2.0
 	 * Expected Output: Range from 0.0 to 3.0
 	 * Assumptions: N/A
 	 */
 	@Test
-	public void shift_CrossOverZero_Test() {
+	public void shift_ZeroCenteredRangeCrossOverZero_Test() {
 		Range result = Range.shift(r, 2.0);
 		Range expected = new Range(0.0, 3.0);
 		assertEquals(expected, result);
